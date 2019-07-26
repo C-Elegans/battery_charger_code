d1e604676533e3e04af85e11a74c61c1a2e85fd2
Michael Nolan <mtnolan2640@gmail.com>
2019-07-25 21:10:31 -0400

    Add 3 (actually 2) stage battery charger state machine

--- main.c
+++ main.c
@@ -40,11 +40,19 @@
 #define BOOST_THRESHOLD 170 // 17V * 1k/(1k+20k) * 1024/5V
 #define BOOST_DCL_MAX 80 
 
-#define BATT_THRESHOLD 223 // 12V * 1k/(1k+10k) * 1024/5V
 #define BUCK_DCL_MAX 110
 
-#define CURRENT_THRESHOLD 280 // .2A * 0.05 ohm * 9x gain, * 1024/5V
+#define STAGE_1_CURRENT_THRESHOLD 243 // 2.5A * 0.05 ohm * 9x gain, * 1024/5V
+#define STAGE_1_VOLTAGE_THRESHOLD 268 // 14.4V * 1k/(1k+10k) * 1024/5V
 
+#define STAGE_2_CURRENT_THRESHOLD 97 // 3A * 0.05 ohm * 9x gain, * 1024/5V
+#define STAGE_2_CURRENT_MINIMUM 10   // .1A * 0.05 ohm * 9x gain, * 1024/5V
+#define STAGE_2_VOLTAGE_THRESHOLD 256 // 13.8V * 1k/(1k+10k) * 1024/5V
+
+uint16_t current_threshold = STAGE_1_CURRENT_THRESHOLD;
+uint16_t voltage_threshold = STAGE_1_VOLTAGE_THRESHOLD;
+
+//@ assigns ADCON1bits, ADCON0bits;
 void init_adc(void){
     ADCON1bits.ADFM = 1;
     ADCON1bits.ADCS = 0x2; // FOSC/32 - TAd = 1us
@@ -85,7 +93,7 @@ uint16_t read_adc(uint8_t pin){
 /*@
   ensures (boost_voltage < BOOST_THRESHOLD && PWM1DCL < BOOST_DCL_MAX) ==>
                 PWM1DCL >= \old(PWM1DCL);
-  ensures (boost_voltage > BOOST_THRESHOLD && PWM1DCL > 5) ==>
+  ensures (boost_voltage > BOOST_THRESHOLD && PWM1DCL > 0) ==>
                 PWM1DCL <= \old(PWM1DCL);
   ensures 5 < \old(PWM1DCL) < BOOST_DCL_MAX ==>
           0 <= PWM1DCL <= BOOST_DCL_MAX;
@@ -96,8 +104,8 @@ void update_boost(uint16_t boost_voltage){
        PWM1DCL += 1;
        
     }
-    if(boost_voltage > BOOST_THRESHOLD && PWM1DCL > 5){
-        PWM1DCL = 0;
+    if(boost_voltage > BOOST_THRESHOLD && PWM1DCL >= 2){
+        PWM1DCL -= 2;
     }
     PWM1LDCONbits.LDA = 1;
 }
@@ -107,17 +115,65 @@ uint16_t buck_counter = 63;
 /*@ 
      
   assigns PWM2DCL, PWM2LDCONbits, ADCON0bits;
-  ensures (buck_voltage < BATT_THRESHOLD && current_sense < CURRENT_THRESHOLD) ==> PWM2DCL >= \old(PWM2DCL);
-  ensures (buck_voltage > BATT_THRESHOLD || current_sense > CURRENT_THRESHOLD) ==> PWM2DCL <= \old(PWM2DCL);
+  ensures (buck_voltage < voltage_threshold && current_sense < current_threshold) ==> PWM2DCL >= \old(PWM2DCL);
+  ensures (buck_voltage > voltage_threshold || current_sense > current_threshold) ==> PWM2DCL <= \old(PWM2DCL);
 */
 void update_buck(uint16_t buck_voltage, uint16_t current_sense){
-    if(buck_voltage < BATT_THRESHOLD && PWM2DCL < BUCK_DCL_MAX && current_sense < CURRENT_THRESHOLD){
+    if(buck_voltage < voltage_threshold && PWM2DCL < BUCK_DCL_MAX && current_sense < current_threshold){
         PWM2DCL += 1;
     }
-    else if((buck_voltage > BATT_THRESHOLD || current_sense > CURRENT_THRESHOLD) && PWM2DCL > 0){
+    else if((buck_voltage > voltage_threshold || current_sense > current_threshold) && PWM2DCL > 0){
         PWM2DCL -= 1;
     }
     PWM2LDCONbits.LDA = 1;
+}
+
+enum state{
+  CONSTANT_CURRENT,
+  CONSTANT_VOLTAGE,
+  DONE
+};
+enum state state = CONSTANT_CURRENT;
+
+/*@ assigns current_threshold, voltage_threshold, state;
+  behavior cc:
+    assumes state == CONSTANT_CURRENT;
+    ensures voltage_threshold == STAGE_1_VOLTAGE_THRESHOLD;
+    ensures current_threshold == STAGE_1_CURRENT_THRESHOLD;
+    ensures buck_voltage > STAGE_1_VOLTAGE_THRESHOLD - 2 ==> state == CONSTANT_VOLTAGE;
+  behavior cv:
+    assumes state == CONSTANT_VOLTAGE;
+    ensures voltage_threshold == STAGE_2_VOLTAGE_THRESHOLD;
+    ensures current_threshold == STAGE_2_CURRENT_THRESHOLD;
+    ensures current_sense < STAGE_2_CURRENT_MINIMUM ==> state == DONE;
+  behavior done:
+    assumes state != CONSTANT_CURRENT && state != CONSTANT_VOLTAGE;
+    ensures voltage_threshold == 0;
+    ensures current_threshold == 0;
+  complete behaviors;
+  disjoint behaviors;
+ */
+void state_machine(uint16_t buck_voltage, uint16_t current_sense){
+  switch(state){
+  case CONSTANT_CURRENT:
+    voltage_threshold = STAGE_1_VOLTAGE_THRESHOLD;
+    current_threshold = STAGE_1_CURRENT_THRESHOLD;
+    if(buck_voltage > STAGE_1_VOLTAGE_THRESHOLD - 2){
+      state = CONSTANT_VOLTAGE;
+    }
+    break;
+  case CONSTANT_VOLTAGE:
+    voltage_threshold = STAGE_2_VOLTAGE_THRESHOLD;
+    current_threshold = STAGE_2_CURRENT_THRESHOLD;
+    if(current_sense < STAGE_2_CURRENT_MINIMUM){
+      state = DONE;
+    }
+    break;
+  default:
+    voltage_threshold = 0;
+    current_threshold = 0;
+    break;
+  }
 }
 
 unsigned char dummy = 5;
@@ -134,9 +190,9 @@ void main(void) {
     setup_pps();
     while(1) {
         uint16_t boost_voltage = read_adc(BOOST_SENSE_ADC);
-	uint16_t buck_voltage = read_adc(BATT_SENSE_ADC);
-	uint16_t current_sense = read_adc(ISENSE_ADC);
-	update_boost(boost_voltage);
+        uint16_t buck_voltage = read_adc(BATT_SENSE_ADC);
+        uint16_t current_sense = read_adc(ISENSE_ADC);
+        update_boost(boost_voltage);
         update_buck(buck_voltage, current_sense);
     }
     return;
